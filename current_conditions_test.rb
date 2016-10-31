3959918758c105b687a63e891f94da7201d68836
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-10-31 13:41:18 -0400

    created test for get method on currend conditions with stub_request

--- current_conditions_test.rb
+++ current_conditions_test.rb
@@ -12,4 +12,19 @@ class CurrentConditionsTest < Minitest::Test
     assert CurrentConditions
   end
 
+  def test_can_get_current_conditions
+    stub_request(
+      :get,
+      "http://api.wunderground.com/api/#{ENV["WUNDERGROUND_KEY"]}/conditions/q/20815"
+    ).to_return(
+      :status => 200,
+      :body => File.read("./responses/chevy_chase_conditions.json"),
+      :headers => {}
+    )
+
+    chevy_chase = CurrentConditions.new.get(20815)
+    assert_equal 59.0, chevy_chase["temp_f"]
+
+  end
+
 end
